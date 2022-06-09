e89ced7a59df2b92ec0540c18e642a7d9ce3af52
Narin Ahir <ahirnm266@gmail.com>
2022-06-10 03:15:49 +0530

    updated

--- loginsystem/urls.py
+++ loginsystem/urls.py
@@ -12,11 +12,11 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django import views
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
+# """
+# from django import views
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.contrib import admin
 from django.urls import path
 from . import views
 
@@ -26,5 +26,5 @@ urlpatterns = [
     path('login/', views.loginnow),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
