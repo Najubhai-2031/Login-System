7f1529f84d12e0e24a4e7ecbe03d0fd44d66906b
Narin Ahir <ahirnm266@gmail.com>
2022-06-10 02:22:39 +0530

    updated

--- loginsystem/urls.py
+++ loginsystem/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', signupnow),
     path('login/', loginnow),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
