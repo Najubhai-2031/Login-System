5dba4ef44ced8a42179d47778355963dc8697697
Narin Ahir <ahirnm266@gmail.com>
2022-06-10 01:17:25 +0530

    first

---
+++ signup/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+import mysql.connector as sql
+# aa nichena globle variable chhe
+fn=''
+ln=''
+sex=''
+em=''
+pwd=''
+
+
+# Create your views here.
+
+def signupnow(request):
+    global fn,ln,sex,em,pwd
+    if request.method=="POST":
+        m=sql.connect(host="localhost", user="root", passwd="20312661999", database='website')
+        cursor=m.cursor()
+        d=request.POST
+        for key,value in d.items():
+            if key=="first_name":
+                fn=value
+            if key=="last_name":
+                ln=value
+            if key=="sex":
+                sex=value
+            if key=="email":
+                em=value
+            if key=="password":
+                pwd=value
+        
+        c="insert into users Values('{}','{}','{}','{}','{}')".format(fn,ln,sex,em,pwd)
+        cursor.execute(c)
+        m.commit()
+
+    return render(request, 'signup_page.html')
