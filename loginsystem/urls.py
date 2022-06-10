ff75a4cf9c49483c8953161101d5f1f1628bb0a4
Narin Ahir <ahirnm266@gmail.com>
2022-06-10 12:50:29 +0530

    Updated

--- loginsystem/urls.py
+++ loginsystem/urls.py
@@ -13,17 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 # """
-# from django import views
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from django.contrib import admin
+
+from django import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', views.loginnow),
     path('signup/', views.signupnow),
     path('login/', views.loginnow),
+    # path('login/', include('login.urls')),
 ]
 
 # if settings.DEBUG:
